b4462e4417f42fea1d8c66d8892f7b4ba6097bec
YuraDnistriansky <yura.dniester@gmail.com>
2025-11-20 21:39:04 +0200

    add mini comment

--- src/readData.cpp
+++ src/readData.cpp
@@ -5,11 +5,11 @@
 extern Adafruit_BME280 bme;
 
 float readTemperature() {
-    float temperature = bme.readTemperature() + 30.00;
+    float temperature = bme.readTemperature() + 30.00; // + 30 for motor
     return temperature;
 }
 
 float readPressure() {
-    float pressure = (bme.readPressure() / 100.0F); // hPa
+    float pressure = (bme.readPressure() / 100.0F); // hPa // But for motor -> kPa
     return pressure;
 }
